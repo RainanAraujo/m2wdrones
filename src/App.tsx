25ab4c1900777c1e66048a72e31649cfc46df0ae
rainanaraujo <rainanaraaujo@gmail.com>
2025-02-06 13:05:22 -0300

    website complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,56 @@ import {
   EnvelopeSimple,
   FacebookLogo,
   InstagramLogo,
+  List,
   MapPin,
   WhatsappLogo,
 } from "@phosphor-icons/react";
 
+import t50Especificacoes from "@/data/t50-especificacoes.json";
+
+import t25Especificacoes from "@/data/t25-especificacoes.json";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./components/sheet";
+import React from "react";
+
 function App() {
+  const [menuOpen, setMenuOpen] = React.useState(false);
   return (
     <div className="font-display">
-      <div className="h-screen grid grid-rows-[40px_80px_1fr]">
-        <nav className="flex bg-gray-200">
+      <div className="h-screen grid grid-rows-[40px_80px_1fr] max-md:grid-rows-[80px_1fr]">
+        <nav className="flex bg-gray-200 max-md:hidden">
           <div className="mx-auto flex justify-between items-center p-2 px-8 w-full text-sm">
             <ul className="flex gap-6">
               <li>
-                <a className="flex items-center gap-2 cursor-pointer">
+                <a
+                  className="flex items-center gap-2 cursor-pointer"
+                  href="https://wa.me/5599984770250"
+                  target="_blank"
+                >
                   <WhatsappLogo size={24} className="text-green-primary" />
                   +55 99 98477-0250
                 </a>
               </li>
               <li>
-                <a className="flex items-center gap-2 cursor-pointer">
+                <a
+                  className="flex items-center gap-2 cursor-pointer"
+                  href="https://maps.app.goo.gl/9HWE7MmG1hff98fw8"
+                  target="_blank"
+                >
                   <MapPin size={24} className="text-green-primary" />
                   Av. Rio Branco, 924, Centro - Pedreiras-MA
                 </a>
               </li>
               <li>
-                <a className="flex items-center gap-2 cursor-pointer">
+                <a
+                  className="flex items-center gap-2 cursor-pointer"
+                  href="mailto:m2wdrones@gmail.com"
+                >
                   <EnvelopeSimple size={24} className="text-green-primary" />
                   m2wdrones@gmail.com
                 </a>
@@ -38,16 +63,19 @@ function App() {
               <a href="https://www.facebook.com/m2wdrones" target="_blank">
                 <InstagramLogo size={24} />
               </a>
-              <a href="https://www.instagram.com/m2wdrones" target="_blank">
+              <a
+                href="https://www.facebook.com/profile.php?id=61562405114871"
+                target="_blank"
+              >
                 <FacebookLogo size={24} />
               </a>
             </div>
           </div>
         </nav>
-        <nav className="flex bg-gray-100 ">
-          <div className="max-w-7xl mx-auto flex justify-start items-center w-full p-4 gap-8 px-10">
+        <nav className="flex bg-gray-100">
+          <div className="max-w-7xl mx-auto flex justify-start items-center w-full p-4 gap-8 px-10  max-md:justify-between">
             <img src="/simbol.svg" alt="M2W Drones" className="h-12" />
-            <ul className="flex gap-6">
+            <ul className="flex gap-6 max-md:hidden">
               <li>
                 <a href="#services" className="hover:underline">
                   Serviços
@@ -69,18 +97,58 @@ function App() {
                 </a>
               </li>
             </ul>
+            <Sheet open={menuOpen} onOpenChange={setMenuOpen}>
+              <SheetTrigger asChild className="md:hidden">
+                <button>
+                  <List size={32} />
+                </button>
+              </SheetTrigger>
+              <SheetContent
+                onCloseAutoFocus={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                }}
+              >
+                <ul className="flex gap-10 flex-col mt-20 text-xl ">
+                  <li>
+                    <a href="#services" onClick={() => setMenuOpen(false)}>
+                      Serviços
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#products" onClick={() => setMenuOpen(false)}>
+                      Produtos
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#contact" onClick={() => setMenuOpen(false)}>
+                      Contato
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#about" onClick={() => setMenuOpen(false)}>
+                      Sobre nós
+                    </a>
+                  </li>
+                </ul>
+              </SheetContent>
+            </Sheet>
           </div>
         </nav>
         <main className=" bg-[url(/back_main_section.svg)] bg-cover text-white">
           <div className="flex items-start flex-col justify-end max-w-7xl w-full p-10 mx-auto h-full ">
-            <div className="flex flex-col h-full justify-center  gap-2">
+            <div className="flex flex-col h-full justify-center  gap-2 max-md:items-center  max-md:text-center max-md:w-full">
               <h1 className="text-5xl ">
                 AUTORIZADA <span className="font-extrabold">DJI</span>
               </h1>
               <h2 className="text-xl">A mais alta tecnologia</h2>
-              <button className="mt-4 px-6 py-2 bg-transparent text-white border border-white rounded-full hover:bg-white hover:text-black cursor-pointer transition-all w-fit">
+              <a
+                href="https://wa.me/5599984770250"
+                target="_blank"
+                className="mt-4 px-6 py-2 bg-transparent text-white border border-white rounded-full hover:bg-white hover:text-black cursor-pointer transition-all w-fit"
+              >
                 Entre em contato
-              </button>
+              </a>
             </div>
             <div className="w-full flex justify-center items-center">
               <img src="/logos.svg" alt="logos" />
@@ -129,7 +197,11 @@ function App() {
                 </h4>
                 <p className="text-gray-300">{service.desc}</p>
               </div>
-              <a href="" className="flex gap-2 mt-10">
+              <a
+                className="flex gap-2 mt-10"
+                href="https://wa.me/5599984770250"
+                target="_blank"
+              >
                 <ArrowSquareOut size={24} /> Saiba mais
               </a>
             </div>
@@ -140,13 +212,13 @@ function App() {
         <div className="flex flex-col items-center mb-10">
           <h3 className="text-5xl font-semibold text-center mb-6">Produtos</h3>
           <hr className="border border-green-primary w-10 mb-2" />
-          <span className="text-center text-gray-600">
+          <span className="text-center text-gray-600 px-10">
             Conheça as tecnologias de ponta que oferecemos para você
           </span>
         </div>
 
         <div className="flex flex-col gap-8 max-w-7xl m-auto p-10">
-          <div className="p-20 grid grid-cols-[1fr_1fr] justify-items-center rounded-md bg-white items-center">
+          <div className="p-20 grid grid-cols-[1fr_1fr] justify-items-center rounded-md bg-white items-center max-md:flex max-md:flex-wrap max-md:items-center max-md:text-center  max-md:p-10">
             <img src="/mavic.svg" alt="simbol" />
             <div className="flex flex-col gap-2">
               <h4 className="text-4xl font-semibold mb-4 ">DJI MAVIC 3M</h4>
@@ -157,10 +229,14 @@ function App() {
                 clareza. A gestão da produção agrícola requer precisão e dados,
                 e o Mavic 3M oferece ambos.
               </p>
-              <div className="flex gap-2 itens-center mt-4">
-                <button className="p-2 px-5 border border-blue-500 rounded-full text-blue-500">
+              <div className="flex gap-2 itens-center mt-4 max-md:flex-wrap max-md:justify-center">
+                <a
+                  className="p-2 px-5 border border-blue-500 rounded-full text-blue-500"
+                  href="https://wa.me/5599984770250"
+                  target="_blank"
+                >
                   Entre em contato
-                </button>
+                </a>
                 <button className="flex gap-2  items-center p-2 px-5 text-blue-500">
                   Ver especificações <CaretRight />
                 </button>
@@ -168,7 +244,7 @@ function App() {
             </div>
           </div>
 
-          <div className="p-20 grid grid-cols-[1fr_1fr]  justify-items-center rounded-md bg-white items-center">
+          <div className="p-20 grid grid-cols-[1fr_1fr]  justify-items-center rounded-md bg-white items-center  max-md:flex max-md:flex-wrap-reverse max-md:items-center max-md:text-center max-md:p-10">
             <div className="flex flex-col gap-2">
               <h4 className="text-4xl font-semibold mb-4 ">DJI AGRAS T50</h4>
               <p className="text-gray-600">
@@ -184,19 +260,48 @@ function App() {
                 pulverização e dispersão, garantindo operações estáveis e
                 desempenho estável.
               </p>
-              <div className="flex gap-2 itens-center mt-4">
-                <button className="p-2 px-5 border border-blue-500 rounded-full text-blue-500">
+              <div className="flex gap-2 itens-center mt-4 max-md:flex-wrap  max-md:justify-center">
+                <a
+                  className="p-2 px-5 border border-blue-500 rounded-full text-blue-500"
+                  href="https://wa.me/5599984770250"
+                  target="_blank"
+                >
                   Entre em contato
-                </button>
-                <button className="flex gap-2  items-center p-2 px-5 text-blue-500">
-                  Ver especificações <CaretRight />
-                </button>
+                </a>
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <button className="flex gap-2  items-center p-2 px-5 text-blue-500">
+                      Ver especificações <CaretRight />
+                    </button>
+                  </SheetTrigger>
+                  <SheetContent className="overflow-y-auto">
+                    <SheetHeader>
+                      <SheetTitle>Especificações</SheetTitle>
+                    </SheetHeader>
+                    {t50Especificacoes.map((especificacao, i) => {
+                      return Object.entries(especificacao).map(
+                        ([categoriaNome, especificacoes]) => (
+                          <div key={i} className="flex flex-col gap-2 p-4 mb-2">
+                            <h1 className="text-xl mb-2">{categoriaNome}</h1>
+                            {especificacoes.map((item: string) =>
+                              Object.entries(item).map(([chave, valor], k) => (
+                                <div key={k} className="text-sm">
+                                  <strong>{chave}:</strong> {valor}
+                                </div>
+                              ))
+                            )}
+                          </div>
+                        )
+                      );
+                    })}
+                  </SheetContent>
+                </Sheet>
               </div>
             </div>
             <img src="/t50.svg" alt="simbol" />
           </div>
 
-          <div className="p-20 grid grid-cols-[1fr_1fr] justify-items-center rounded-md bg-white items-center">
+          <div className="p-20 grid grid-cols-[1fr_1fr] justify-items-center rounded-md bg-white items-center  max-md:flex max-md:flex-wrap max-md:items-center max-md:text-center  max-md:p-10">
             <img src="/t25.svg" alt="simbol" />
             <div className="flex flex-col gap-2">
               <h4 className="text-4xl font-semibold mb-4">DJI AGRAS T25</h4>
@@ -212,13 +317,42 @@ function App() {
                 aéreo até pulverização e dispersão, e se destaca em uma
                 variedade de terrenos
               </p>
-              <div className="flex gap-2 itens-center mt-4">
-                <button className="p-2 px-5 border border-blue-500 rounded-full text-blue-500">
+              <div className="flex gap-2 itens-center mt-4 max-md:flex-wrap  max-md:justify-center">
+                <a
+                  className="p-2 px-5 border border-blue-500 rounded-full text-blue-500"
+                  href="https://wa.me/5599984770250"
+                  target="_blank"
+                >
                   Entre em contato
-                </button>
-                <button className="flex gap-2  items-center p-2 px-5 text-blue-500">
-                  Ver especificações <CaretRight />
-                </button>
+                </a>
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <button className="flex gap-2  items-center p-2 px-5 text-blue-500">
+                      Ver especificações <CaretRight />
+                    </button>
+                  </SheetTrigger>
+                  <SheetContent className="overflow-y-auto">
+                    <SheetHeader>
+                      <SheetTitle>Especificações</SheetTitle>
+                    </SheetHeader>
+                    {t25Especificacoes.map((especificacao, i) => {
+                      return Object.entries(especificacao).map(
+                        ([categoriaNome, especificacoes]) => (
+                          <div key={i} className="flex flex-col gap-2 p-4 mb-2">
+                            <h1 className="text-xl mb-2">{categoriaNome}</h1>
+                            {especificacoes.map((item: string) =>
+                              Object.entries(item).map(([chave, valor], k) => (
+                                <div key={k} className="text-sm">
+                                  <strong>{chave}:</strong> {valor}
+                                </div>
+                              ))
+                            )}
+                          </div>
+                        )
+                      );
+                    })}
+                  </SheetContent>
+                </Sheet>
               </div>
             </div>
           </div>
@@ -229,15 +363,23 @@ function App() {
           <h3 className="text-5xl font-semibold text-center mb-6">Contato</h3>
           <hr className="border border-green-primary w-10 mb-2" />
         </div>
-        <div className="flex gap-4 items-center justify-center">
-          <div className="flex gap-4 bg-[#F5F8F9] p-8 ">
+        <a
+          href="https://wa.me/5599984770250"
+          target="_blank"
+          className="flex gap-4 items-center justify-center flex-wrap max-md:jus"
+        >
+          <div className="flex gap-4 bg-[#F5F8F9] p-8 max-md:w-full">
             <WhatsappLogo className="text-green-primary" size={48} />
             <div className="">
               <span className="text-xs text-gray-400">Whatsapp</span>
               <p className="font-semibold">+55 99 98477-0250</p>
             </div>
           </div>
-          <div className="flex gap-4 bg-[#F5F8F9] p-8 ">
+          <a
+            href="https://maps.app.goo.gl/qZYBVaC5q5aUMrBi8"
+            target="_blank"
+            className="flex gap-4 bg-[#F5F8F9] p-8  max-md:w-full"
+          >
             <MapPin className="text-green-primary" size={48} />
             <div className="">
               <span className="text-xs text-gray-400">Endereço</span>
@@ -245,20 +387,27 @@ function App() {
                 Av. Rio Branco, 924, Centro Pedreiras-MA
               </p>
             </div>
-          </div>
-          <div className="flex gap-4 bg-[#F5F8F9] p-8 ">
+          </a>
+          <a
+            href="mailto:m2wdrones@gmail.com"
+            target="_blank"
+            className="flex gap-4 bg-[#F5F8F9] p-8  max-md:w-full"
+          >
             <EnvelopeSimple className="text-green-primary" size={48} />
             <div className="">
               <span className="text-xs text-gray-400">Email</span>
               <p className="font-semibold">m2wdrones@gmail.com</p>
             </div>
-          </div>
-        </div>
+          </a>
+        </a>
       </section>
-      <section id="about" className="bg-[#F5F8F9] grid grid-cols-[1fr_1fr]">
+      <section
+        id="about"
+        className="bg-[#F5F8F9] grid grid-cols-[1fr_1fr] max-md:flex max-md:text-center"
+      >
         <div className="bg-[url(/fachada.svg)] bg-cover" />
-        <div className="p-32 flex flex-col gap-4">
-          <h1 className="text-5xl font-semibold  mb-6">Sobre nós</h1>
+        <div className="p-32 flex flex-col gap-4 max-md:px-10">
+          <h1 className="text-5xl font-semibold  mb-6 ">Sobre nós</h1>
           <p className="text-3xl text-green-primary">
             Somos a M2W Drones, representantes autorizados da DJI Agriculture no
             Maranhão!
@@ -270,12 +419,16 @@ function App() {
             agrícolas, além de serviços de assistência técnica com oficina
             própria, garantindo suporte especializado e pós-venda de qualidade.
           </p>
-          <button className="p-2 px-5 border border-blue-500 rounded-full text-blue-500 w-fit mt-10">
+          <a
+            className="p-2 px-5 border border-blue-500 rounded-full text-blue-500 w-fit mt-10 max-md:m-auto"
+            href="https://wa.me/5599984770250"
+            target="_blank"
+          >
             Entre em contato
-          </button>
+          </a>
         </div>
       </section>
-      <footer className="h-[80px] flex justify-between items-center px-20">
+      <footer className="h-[80px] flex justify-between items-center px-20 max-md:px-10 gap-8">
         <img src="simbol.svg" alt="simbol" className="h-14 " />
         <span className="text-gray-500">
           M2W Drones Solution todos os direitos reservados
